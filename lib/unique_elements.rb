b077272423b5662bd66f1d036a292bad326958d6
rynium <ryanstantz@gmail.com>
2016-02-25 03:05:45 +0000

    Done

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,19 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
-  
+    empty = {}
+    unique = []
+    
+    i = 0 
+    while i < arr.length
+        if empty.has_key?(arr[i]) == true
+            empty[arr[i]] +=1
+        else
+            empty[arr[i]] = 1
+        end
+    i += 1
+    end
+    
+    empty.each {|k,v| unique.push(k) if v<2}
+    unique
 end
